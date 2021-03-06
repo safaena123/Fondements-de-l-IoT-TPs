c3b6af7cfab8936c0501bf3f5b04573363f24409
Safaena123 <safaenabet7@gmail.com>
2021-03-06 15:33:58 +0100

    Clignotement de deux LEDs

---
+++ TP1/TP1-1/TP1-1.ino
@@ -0,0 +1,19 @@
+
+int redLED = 13;
+int greenLED = 12;
+
+void setup() {
+  // initialize digital pins as an output.
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(redLED, HIGH);   
+  delay(1000);                       // wait for a second
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, HIGH); 
+  delay(1000);
+  digitalWrite(greenLED, LOW);
+}
